352878de18467a9222e21a7c18be435e770b9b37
Szabolcs Berko <szberko@gmail.com>
2019-06-18 20:22:28 +0200

    make FileUtility more straightforward

---
+++ src/main/java/com/szberko/ditributedtracing/exception/GraphCannotCreatedException.java
@@ -0,0 +1,8 @@
+package com.szberko.ditributedtracing.exception;
+
+public class GraphCannotCreatedException extends Exception {
+
+    public GraphCannotCreatedException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/szberko/ditributedtracing/utility/FileUtility.java
+++ src/main/java/com/szberko/ditributedtracing/utility/FileUtility.java
@@ -1,29 +1,37 @@
 package com.szberko.ditributedtracing.utility;
 
-import com.szberko.ditributedtracing.model.Node;
+import com.szberko.ditributedtracing.exception.GraphCannotCreatedException;
 import com.szberko.ditributedtracing.model.Edge;
 import com.szberko.ditributedtracing.model.Graph;
+import com.szberko.ditributedtracing.model.Node;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileUtility {
 
+    public static Graph provideGraph(final String filePath) throws IOException, GraphCannotCreatedException {
+        return readFromFile(filePath).map(FileUtility::separateEdges)
+                .map(FileUtility::parse)
+                .map(Graph::new)
+                .findAny()
+                .orElseThrow(() -> new GraphCannotCreatedException("Cannot create graph from file"));
+    }
+
+
     public static Graph parseGraph(String input){
-        final List<String> connections = separateEdges(input);
+        final Stream<String> connections = separateEdges(input);
 
         final Map<String, Node> nodes = parse(connections);
         return new Graph(nodes);
     }
 
-    private static Map<String, Node> parse(List<String> connections){
+    private static Map<String, Node> parse(Stream<String> connections){
         Map<String, Node> nodes = new HashMap<>();
         connections.forEach(connection -> {
             Node start = nodes.computeIfAbsent("" + connection.charAt(0), Node::new);
@@ -39,14 +47,13 @@ public class FileUtility {
         return nodes;
     }
 
-    protected static List<String> separateEdges(String input){
+    private static Stream<String> separateEdges(String input){
         return Stream.of(input).map(string -> string.split(","))
                 .flatMap(Arrays::stream)
-                .map(String::trim)
-                .collect(Collectors.toList());
+                .map(String::trim);
     }
 
-    protected static Stream<String> readFromFile(final String filePath) throws IOException {
+    private static Stream<String> readFromFile(final String filePath) throws IOException {
         return Files.lines(Paths.get(filePath));
     }
 }
